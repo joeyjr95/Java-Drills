a44144883a68bc16ff300638ac21dd759e13e644
Joey Romo <joeyjr95@gmail.com>
2020-02-18 16:39:47 -0500

    java drills

---
+++ simple_string_formatting/Solution/src/com/thinkful/app/Solution.java
@@ -0,0 +1,15 @@
+package com.thinkful.app;
+
+
+public class Solution {
+    public static void main(String[] args) {
+        String movieOne = "1. The Lord of the Rings: The Fellowship of the Rings";
+
+        String castOne = "Viggo Mortensen, Elijah Wood";
+
+        String movieTwo = "2. In Bruges";
+
+        String castTwo = "Colin Ferrel, Voldemort, and Mad eyed Moody";
+        System.out.println(movieOne + " " + castOne + "\n" + movieTwo + " " + castTwo);
+    }
+}
